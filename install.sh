5689b320548b9d7c997ee66eab83eae2b1e6ca99
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-09-28 23:33:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-clear
+echo
+echo "Tunggu Sampai Proses Selesai Ya Anjing, Sabar..!"
+echo "Kalo Mau Nyolong Ngaca Dulu Ngentot"
+echo "Ucapkan Yayan Ganteng 3x Biar Work Xixixi:v"
+echo
+pkg install python
+pkg install ruby
+gem install lolcat
 pkg install figlet
 clear
 figlet YayanXD | lolcat
-sleep 2
 clear
-echo
-echo "Tunggu sampai proses selesai ya anjing, SABAR..!"
-echo "Kalo mau nyolong ngaca dulu ngentot"
-echo "Ucapkan yayan ganteng 3X biar work xixixi:v"
-echo 
+figlet YayanXD | lolcat
+clear
+figlet YayanXD | lolcat
 cd $HOME
 pip2 install requests
 pip2 install mechanize

91c810f179a85613ffbce7c0ae183b62723ffb33
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-09-28 09:36:37 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,29 @@
+#bin/bash
+clear
+pkg install figlet
+clear
+figlet YayanXD | lolcat
+sleep 2
+clear
+echo
+echo "Tunggu sampai proses selesai ya anjing, SABAR.....!"
+echo "Kalo mau nyolong ngaca dulu ngentot"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo 
+pkg update
+clear
+pkg upgrade
+clear
+pkg install python2
+clear
+pkg install git
+clear
+git clone https://github.com/Yayan-XD/Silent
+clear
+pip2 install requests
+clear
+pip2 install mechanize 
+clear
+echo "selesai"
+clear
+python2 Silent.py

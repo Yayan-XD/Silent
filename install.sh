f4f870ad9baf8754f63d9b7037f7bc8312261426
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-09-28 18:16:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ figlet YayanXD | lolcat
 sleep 2
 clear
 echo
-echo "Tunggu sampai proses selesai ya anjing, SABAR..!"
+echo "Tunggu sampai proses selesai ya anjing, SABAR.....!"
 echo "Kalo mau nyolong ngaca dulu ngentot"
 echo "Ucapkan yayan ganteng 3X biar work xixixi:v"
 echo 
+cd $HOME
 pip2 install requests
-clear
 pip2 install mechanize

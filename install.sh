cbc24523ce05087a5a345fbee20c3bad35cbd973
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-09-28 23:06:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ figlet YayanXD | lolcat
 sleep 2
 clear
 echo
-echo "Tunggu sampai proses selesai ya anjing, SABAR.....!"
+echo "Tunggu sampai proses selesai ya anjing, SABAR..!"
 echo "Kalo mau nyolong ngaca dulu ngentot"
 echo "Ucapkan yayan ganteng 3X biar work xixixi:v"
 echo 

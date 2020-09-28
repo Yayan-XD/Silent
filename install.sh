6fbb557c1dfd4ed7b4555afb907f546d17024890
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-09-28 15:55:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,6 @@ echo "Tunggu sampai proses selesai ya anjing, SABAR.....!"
 echo "Kalo mau nyolong ngaca dulu ngentot"
 echo "Ucapkan yayan ganteng 3X biar work xixixi:v"
 echo 
-pkg update
-clear
-pkg upgrade
-clear
-pkg install python2
-clear
-pkg install git
-clear
-git clone https://github.com/Yayan-XD/Silent
-clear
 pip2 install requests
 clear
 pip2 install mechanize 
